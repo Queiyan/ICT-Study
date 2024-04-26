be05123f57a9856612bd95504fae8dbf7356a6ee
ict01-17 <ict01-17@DESKTOP-I020VA5>
2024-04-26 09:38:11 +0900

    add 07-02

--- java_03/src/ch07/_02_Main.java
+++ java_03/src/ch07/_02_Main.java
@@ -3,8 +3,13 @@ package ch07;
 public class _02_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("=== 자율 주행하는 자동차 ===");
+		_02_Car myCar = new _02_AICar();
+		myCar.run();
+		
+		System.out.println("=== 사람이 운전하는 자동차 ===");
+		_02_Car hisCar = new _02_ManualCar();
+		hisCar.run();
 	}
 
 }
